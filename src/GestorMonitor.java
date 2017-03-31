dc63676abc379ec71e6eb4aa3e2cfe98dfa7e1cf
Gustavo Gonzalez <gustavog_gs@hotmail.com>
2017-03-31 15:50:16 -0300

    Añadiendo prototipos de petodos del gestor

--- src/GestorMonitor.java
+++ src/GestorMonitor.java
@@ -3,5 +3,11 @@ import java.util.concurrent.Semaphore;
 public class GestorMonitor {
 	
 	final Semaphore entrada_monitor = new Semaphore(1);
+	private boolean k;
+	private RedPetri red = new RedPetri();
+	
+	public void dispararTransicion(){
+		
+	}
 
 }
