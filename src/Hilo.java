3d85d456bc2e5c75394bf035eb2491a0afc6df7f
Facundo Maero <facundojmaero@gmail.com>
2017-03-31 17:09:35 -0300

    Primeros metodos hilo.java

--- src/Hilo.java
+++ src/Hilo.java
@@ -1,12 +1,34 @@
 
 public class Hilo implements Runnable {
 	
+	GestorMonitor gestor;
+	int transicionActual;
+	int[] transiciones;
 	
+	public Hilo(int[] transiciones, GestorMonitor gestor){
+		this.gestor = gestor;
+		this.transiciones = transiciones;
+		for (int i = 0; i < transiciones.length; i++) {
+			if (transiciones[i] != -1){
+				transicionActual = i;
+				break;
+			}
+		} 
+		 
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		gestor.dispararTransicion(transicionActual);
+		siguienteTransicion();
+	}
+	
+	public int getTransicionActual(){
+		return transicionActual;
+	}
+	
+	public void siguienteTransicion(){
+		transicionActual = transiciones[transicionActual];
 	}
 
 }
